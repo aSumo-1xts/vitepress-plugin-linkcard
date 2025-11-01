82a6e8fe2987e5dd02a770dd8a9752858a7a87df
ｱｽﾓ <1xtelescope@gmail.com>
2025-11-01 15:08:13 +0900

    feat: github.comへの例外処理を実装

--- src/assemble/html.ts
+++ src/assemble/html.ts
@@ -26,22 +26,38 @@ export const generateCardDomFragment: CardDomRender = (data, options) => {
     options.bgColor || "#7d7d7d00"
   );
   const url = options.href || "";
-  const domain = new URL(url).origin
-    .replace(/^https?:\/\//, "")
-    .replace(/^www\./, "");
+  const domain =
+    new URL(url).origin.replace(/^https?:\/\//, "").replace(/^www\./, "") ||
+    "Unknown domain";
+
+  let title = data.title;
+  let description = data.description;
+
+  // Special handling for gitub.com
+  if (domain == "github.com") {
+    title = data.title?.split(":")[0].replace("GitHub - ", "") || "No title";
+    description =
+      description?.replace(` - ${title}`, "").replace(
+        `Contribute to ${title} development by creating an account on GitHub.`, // 定型句
+        ""
+      ) || "";
+  } else {
+    title = data.title || "No title";
+    description = data.description || "";
+  }
 
   return `<span style="display:block;">
   <a ${aa.rel} ${aa.target} ${aa.href} ${aa.title} ${style.a}>
     <span ${inject(style.container, classes.container)}>
       <span ${inject(style.texts, classes.texts)}>
         <span ${inject(style.title, classes.title)}>
-          ${escapeHTML(data.title || "")}
+          ${escapeHTML(title)}
         </span>
         <span ${inject(style.domain, classes.domain)}>
-          ${escapeHTML(domain || "(Unknown domain)")}
+          ${escapeHTML(domain)}
         </span>
         <span ${inject(style.description, classes.description)}>
-          ${escapeHTML(data.description || "")}
+          ${escapeHTML(description)}
         </span>
       </span>
       <img src="${data?.logo}" ${inject(style.img, classes.img)}/>
